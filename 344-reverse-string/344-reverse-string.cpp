e344de920eba3cef599a3f02f46efbbdf7c8a9ed
Murtaza Lightwala <45698122+Murtuzz@users.noreply.github.com>
2022-09-08 22:59:49 +0530

    Time: 44 ms (28.37%), Space: 23.2 MB (40.93%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        return reverse(s.begin(), s.end());   
+        int len = s.size() - 1 ;
+        for(int i = 0; i <= len/2; i++){
+            char temp = s[i];
+            s[i] = s[len - i];
+            s[len - i] = temp;
+        }
     }
 };
