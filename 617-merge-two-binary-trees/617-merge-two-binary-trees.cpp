a29b3e6f47db2a5f64c2574e50918c4465051e44
Murtaza Lightwala <45698122+Murtuzz@users.noreply.github.com>
2022-09-13 01:18:59 +0530

    Time: 90 ms (9.65%), Space: 32.3 MB (87.18%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
@@ -1,49 +1,13 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void traversal(TreeNode* root1, TreeNode* root2) {
-        if (!root1->left && !root1->right && !root2->left && !root2->right) {
-            return;
-        } 
-        
-        if (root1->left && root2->left) {
-            root1->left->val += root2->left->val;
-        } else if (root2->left) {
-            root1->left = new TreeNode(root2->left->val); 
-        }
-        
-        if (root1->right && root2->right) {
-            root1->right->val += root2->right->val;
-        } else if (root2->right) {
-            root1->right = new TreeNode(root2->right->val); 
-        }
-        if (root2->left) { 
-            traversal(root1->left,root2->left);
-        }
-        if (root2->right) {
-            traversal(root1->right,root2->right);
-        }
-    }
-    
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
-        if (!root1) {
+        if(root1==NULL)
             return root2;
-        } else if (!root2) {
+        if(root2==NULL)
             return root1;
-        }
-        
-        root1->val += root2->val;
-        traversal(root1, root2);
+        root1->val+=root2->val;
+        root1->left=mergeTrees(root1->left,root2->left);
+        root1->right=mergeTrees(root1->right,root2->right);
         return root1;
     }
 };
