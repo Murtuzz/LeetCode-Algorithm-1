a97cc77497c11a48e298b5754065c090d4252ec0
Murtaza Lightwala <45698122+Murtuzz@users.noreply.github.com>
2022-09-15 00:57:22 +0530

    Time: 2 ms (66.80%), Space: 6.1 MB (9.15%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,11 +1,19 @@
 class Solution {
-public:
-    int fib(int n) {
+public:    
+    int fib(int n, vector<int> &mem) {
         if (n == 0) {
             return 0;
         } else if (n == 1) {
             return 1;
+        } else if (mem[n]) {
+            return mem[n];
         }
-        return fib (n - 1) + fib (n - 2);
+        return mem[n] = fib (n - 1, mem) + fib (n - 2, mem);
     }
+    
+    int fib(int n) {
+        vector<int>mem(n+1);
+        return fib(n, mem);
+    }
+    
 };
