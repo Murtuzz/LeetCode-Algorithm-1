8c171d4e0d79858fe0203c427590a74c0d31061e
Murtaza Lightwala <45698122+Murtuzz@users.noreply.github.com>
2022-09-18 10:30:47 +0530

    Time: 3 ms (60.82%), Space: 5.9 MB (70.97%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -1,20 +1,9 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        int count = 0;
-        while (n > 0) {
-            if (n&1) {
-                count++;
-            }
-            if (count > 1) {
-                return false;
-            }
-            n = n >> 1;
-        }
-        if (count == 1) {
-            return true;
-        } else {
+        if (n <= 0) {
             return false;
         }
+        return !(n & (n-1));    
     }
 };
