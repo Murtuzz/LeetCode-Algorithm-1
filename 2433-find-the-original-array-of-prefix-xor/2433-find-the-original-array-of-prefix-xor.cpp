693c79bd3786d279344cf9b23be7ebcc22d11dbc
Murtaza Lightwala <45698122+Murtuzz@users.noreply.github.com>
2022-10-12 02:04:41 +0530

    Time: 137 ms (85.27%), Space: 76.1 MB (63.68%) - LeetHub

---
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> findArray(vector<int>& pref) {
+        int len = pref.size();
+        for (int i = len - 1; i > 0; i--) {
+            pref[i] = pref[i] ^ pref[i-1];
+        }
+        return pref;
+    }
+};
