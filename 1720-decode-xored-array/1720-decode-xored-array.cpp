190215b03d60ba44e4c1d81daa320218413a7ecb
Murtaza Lightwala <45698122+Murtuzz@users.noreply.github.com>
2022-10-12 02:53:46 +0530

    Time: 45 ms (70.70%), Space: 24.8 MB (89.77%) - LeetHub

--- 1720-decode-xored-array/1720-decode-xored-array.cpp
+++ 1720-decode-xored-array/1720-decode-xored-array.cpp
@@ -1,5 +1,12 @@
 class Solution {
+    
 public:
+    // *** need this constructor to boost the speed ***  
+    Solution() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+    }
+
     vector<int> decode(vector<int>& encoded, int first) {
         int len = encoded.size();
         vector<int> ans(len + 1);
